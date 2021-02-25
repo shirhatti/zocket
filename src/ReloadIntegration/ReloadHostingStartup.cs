96a5f02cb225d7e9bea66b0cccdcbf4ded98adb4
Sourabh Shirhatti <shirhatti@outlook.com>
2021-02-24 20:42:19 -0600

    Add Https support (#3)
    * Add https on linux
    
    * Add WIndows support
    
    * Rename tls-port to https-port

--- src/ReloadIntegration/ReloadHostingStartup.cs
+++ src/ReloadIntegration/ReloadHostingStartup.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO.Pipes;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 using static Tmds.Linux.LibC;
 
 [assembly: HostingStartup(typeof(ReloadIntegration.ReloadHostingStartup))]
@@ -22,16 +24,20 @@ namespace ReloadIntegration
             }
             ClearStartupHookEnvironmentVariable();
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                uint listenFd;
+                uint listenHttpFd, listenHttpsFd;
                 try
                 {
-                    var listenFdAsString = Environment.GetEnvironmentVariable("ZOCKET_LISTEN_FD");
-                    listenFd = uint.Parse(listenFdAsString);
-                    builder.ConfigureKestrel(options =>
+                    listenHttpFd = uint.Parse(Environment.GetEnvironmentVariable("ZOCKET_LISTEN_HTTP_FD"));
+                    listenHttpsFd = uint.Parse(Environment.GetEnvironmentVariable("ZOCKET_LISTEN_HTTPS_FD"));
+                    builder.ConfigureServices(services =>
                     {
-                        options.ListenHandle(listenFd);
+                        services.PostConfigure<KestrelServerOptions>(options =>
+                        {
+                            options.ListenHandle(listenHttpFd);
+                            options.ListenHandle(listenHttpsFd, options => options.UseHttps());
+                        });
                     });
                 }
                 catch (FormatException)
@@ -40,7 +46,9 @@ namespace ReloadIntegration
                 }
 
                 // We do this to prevent leaking a socket
-                fcntl(Convert.ToInt32(listenFd), F_SETFD, FD_CLOEXEC);
+                fcntl((int)listenHttpFd, F_SETFD, FD_CLOEXEC);
+                fcntl((int)listenHttpsFd, F_SETFD, FD_CLOEXEC);
+
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
@@ -53,18 +61,36 @@ namespace ReloadIntegration
 
                 // ProtocolInformation is usually ~630 bytes. TODO may need a read loop here to make sure we have all bytes
                 var buffer = new byte[1024];
-                var length = namedPipeServer.Read(buffer);
 
+                // Read size, then read SocketInfo
+                var length = namedPipeServer.Read(buffer, 0, sizeof(int));
+                var socketInfoLength = BitConverter.ToInt32(buffer, 0);
+                length = namedPipeServer.Read(buffer, 0, socketInfoLength);
                 var socketInfo = new SocketInformation()
                 {
                     ProtocolInformation = (new Memory<byte>(buffer).Slice(0, length)).ToArray()
                 };
 
                 // Shouldn't need to dispose of socket as Kestrel will dispose for us?
-                var socket = new Socket(socketInfo);
-                builder.ConfigureKestrel(options =>
+                var httpSocket = new Socket(socketInfo);
+
+                // Repeat for second socketInfo
+                length = namedPipeServer.Read(buffer, 0, sizeof(int));
+                socketInfoLength = BitConverter.ToInt32(buffer, 0);
+                length = namedPipeServer.Read(buffer, 0, socketInfoLength);
+                socketInfo = new SocketInformation()
                 {
-                    options.ListenHandle((uint)socket.Handle);
+                    ProtocolInformation = (new Memory<byte>(buffer).Slice(0, length)).ToArray()
+                };
+                var httpsSocket = new Socket(socketInfo);
+
+                builder.ConfigureServices(services =>
+                {
+                    services.PostConfigure<KestrelServerOptions>(options =>
+                    {
+                        options.ListenHandle((uint)httpSocket.Handle);
+                        options.ListenHandle((uint)httpsSocket.Handle, options => options.UseHttps());
+                    });
                 });
             }
         }
--- src/zocket/Program.cs
+++ src/zocket/Program.cs
@@ -24,6 +24,9 @@ namespace Zocket
                     "--port",
                     getDefaultValue: () => 9999,
                     description: "Port to bind to"),
+                new Option<int>(
+                    "--https-port",
+                    getDefaultValue: () => 10000),
                 new Argument<string>(
                     "command",
                     getDefaultValue: () => "dotnet watch run",
@@ -31,129 +34,65 @@ namespace Zocket
                     )
             };
             rootCommand.Description = "zocket";
-            rootCommand.Handler = CommandHandler.Create<int, string>(Listen);
+            rootCommand.Handler = CommandHandler.Create<int, int, string>(Listen);
 
             return await rootCommand.InvokeAsync(args);
         }
 
-        private static Task Listen(int port, string command)
+        private static async Task Listen(int port, int httpsPort, string command)
         {
+            using var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            var parsedCommand = command.Split(' ', 2);
+            ProcessStartInfo psi = parsedCommand.Length switch
+            {
+                1 => new ProcessStartInfo(parsedCommand[0]),
+                2 => new ProcessStartInfo(parsedCommand[0], parsedCommand[1]),
+                _ => default
+            };
+
+            using var httpListenSocket = SetupSocket(port);
+            using var httpsListenSocket = SetupSocket(httpsPort);
+
+            var currentAssembly = Assembly.GetExecutingAssembly().Location;
+            var reloadIntegrationPath = Path.GetFullPath(Path.Combine(currentAssembly, "..", "ReloadIntegration.dll"));
+
+            const string dotnetStartHooksName = "DOTNET_STARTUP_HOOKS";
+            psi.EnvironmentVariables[dotnetStartHooksName] = AddOrAppend(dotnetStartHooksName, reloadIntegrationPath, Path.PathSeparator);
+            const string hostingStartupAssembliesName = "ASPNETCORE_HOSTINGSTARTUPASSEMBLIES";
+            psi.EnvironmentVariables[hostingStartupAssembliesName] = AddOrAppend(hostingStartupAssembliesName, "ReloadIntegration", ';');
+
+            string pipeName = default;
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                return ListenWindows(port, command);
+                pipeName = Guid.NewGuid().ToString();
+                psi.EnvironmentVariables["ZOCKET_PIPE_NAME"] = pipeName;
+
             }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                return ListenLinux(port, command);
+                var duplicatedHttpSocket = httpListenSocket.DuplicateSocketLinux();
+                var duplicatedHttpsSocket = httpsListenSocket.DuplicateSocketLinux();
+
+                psi.EnvironmentVariables["ZOCKET_LISTEN_HTTP_FD"] = duplicatedHttpSocket.DangerousGetHandle().ToInt32().ToString();
+                psi.EnvironmentVariables["ZOCKET_LISTEN_HTTPS_FD"] = duplicatedHttpsSocket.DangerousGetHandle().ToInt32().ToString();
             }
-            return Task.CompletedTask;
-        }
 
-        private static Task ListenLinux(int port, string command)
-        {
-            var exitEvent = new ManualResetEvent(false);
-            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
-            {
-                e.Cancel = true;
-                exitEvent.Set();
-            };
-            var ipEndPoint = new IPEndPoint(IPAddress.Loopback, port);
-            using var listenSocket = new Socket(ipEndPoint.AddressFamily,
-                                                SocketType.Stream,
-                                                ProtocolType.Tcp);
-            listenSocket.Bind(ipEndPoint);
-            var duplicatedSocket = listenSocket.DuplicateSocketLinux();
-            var parsedCommand = command.Split(' ', 2);
-            ProcessStartInfo psi = parsedCommand.Length switch
-            {
-                1 => new ProcessStartInfo(parsedCommand[0]),
-                2 => new ProcessStartInfo(parsedCommand[0], parsedCommand[1]),
-                _ => throw new ArgumentException(),
-            };
-
-            var currentAssembly = Assembly.GetExecutingAssembly().Location;
-            var reloadIntegrationPath = Path.GetFullPath(Path.Combine(currentAssembly, "..", "ReloadIntegration.dll"));
-
-            psi.EnvironmentVariables["ZOCKET_LISTEN_FD"] = duplicatedSocket.DangerousGetHandle().ToInt32().ToString();
-            const string dotnetStartHooksName = "DOTNET_STARTUP_HOOKS";
-            psi.EnvironmentVariables[dotnetStartHooksName] = AddOrAppend(dotnetStartHooksName, reloadIntegrationPath, Path.PathSeparator);
-            const string hostingStartupAssembliesName = "ASPNETCORE_HOSTINGSTARTUPASSEMBLIES";
-            psi.EnvironmentVariables[hostingStartupAssembliesName] = AddOrAppend(hostingStartupAssembliesName, "ReloadIntegration", ';');
-
-            var process = Process.Start(psi);
-            exitEvent.WaitOne();
-
-            process.Terminate();
-            return Task.CompletedTask;
-        }
-
-        private static async Task ListenWindows(int port, string command)
-        {
-            using var cancellationTokenSource = new CancellationTokenSource();
-            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
-            {
-                e.Cancel = true;
-                cancellationTokenSource.Cancel();
-            };
-
-            var parsedCommand = command.Split(' ', 2);
-            ProcessStartInfo psi = parsedCommand.Length switch
-            {
-                1 => new ProcessStartInfo(parsedCommand[0]),
-                2 => new ProcessStartInfo(parsedCommand[0], parsedCommand[1]),
-                _ => throw new ArgumentException(),
-            };
-
-            var currentAssembly = Assembly.GetExecutingAssembly().Location;
-            var reloadIntegrationPath = Path.GetFullPath(Path.Combine(currentAssembly, "..", "ReloadIntegration.dll"));
-
-            using var listenSocket = SetupSocket(port);
-            
-            const string dotnetStartHooksName = "DOTNET_STARTUP_HOOKS";
-            psi.EnvironmentVariables[dotnetStartHooksName] = AddOrAppend(dotnetStartHooksName, reloadIntegrationPath, Path.PathSeparator);
-            const string hostingStartupAssembliesName = "ASPNETCORE_HOSTINGSTARTUPASSEMBLIES";
-            psi.EnvironmentVariables[hostingStartupAssembliesName] = AddOrAppend(hostingStartupAssembliesName, "ReloadIntegration", ';');
-            var pipeName = Guid.NewGuid().ToString();
-            psi.EnvironmentVariables["ZOCKET_PIPE_NAME"] = pipeName;
             using var initialProcess = Process.Start(psi);
 
-            // See if initial process has shutdown at anypoint, if so shutdown zocket.
-            var initialProcessExit = Task.Run(async () =>
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                await initialProcess.WaitForExitAsync();
-                cancellationTokenSource.Cancel();
-            });
+                await ListenWindows(cts, httpListenSocket, httpsListenSocket, pipeName, initialProcess);
+            }
 
-            while (!cancellationTokenSource.IsCancellationRequested)
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                try
-                {
-                    using var namedPipeServer = new NamedPipeServerStream(pipeName,
-                        PipeDirection.InOut,
-                        maxNumberOfServerInstances: 1,
-                        PipeTransmissionMode.Byte,
-                        PipeOptions.Asynchronous);
-                    await namedPipeServer.WaitForConnectionAsync(cancellationTokenSource.Token);
-
-                    var buffer = new byte[16]; // Only need enough for the length of a PID, 16 should be plenty
-                    var length = await namedPipeServer.ReadAsync(buffer, cancellationTokenSource.Token);
-                    var pid = BitConverter.ToInt32(new ReadOnlySpan<byte>(buffer).Slice(0, length));
-
-                    // TODO how can we pass this info into other transports (QUIC) s.t. it duplicates rather than creates?
-
-                    var socketInfo = listenSocket.DuplicateSocketWindows(pid);
-                    await namedPipeServer.WriteAsync(socketInfo.ProtocolInformation, cancellationTokenSource.Token);
-
-                    var backendProcess = Process.GetProcessById(pid);
-
-                    await backendProcess.WaitForExitAsync(cancellationTokenSource.Token);
-                }
-                catch (Exception)
-                {
-                    // Ignore exceptions for now.
-                    // TODO enable a debug log mode.
-                    break;
-                }
+                await initialProcess.WaitForExitAsync(cts.Token);
             }
 
             try
@@ -163,6 +102,54 @@ namespace Zocket
             catch (InvalidOperationException)
             {
                 // process has already exited.
+            }
+        }
+
+        private static async Task ListenWindows(CancellationTokenSource cts, Socket httpListenSocket, Socket httpsListenSocket, string pipeName, Process initialProcess)
+        {
+            // See if initial process has shutdown at anypoint, if so shutdown zocket.
+            var initialProcessExit = Task.Run(async () =>
+            {
+                await initialProcess.WaitForExitAsync();
+                cts.Cancel();
+            });
+            while (!cts.IsCancellationRequested)
+            {
+                try
+                {
+                    using var namedPipeServer = new NamedPipeServerStream(pipeName,
+                        PipeDirection.InOut,
+                        maxNumberOfServerInstances: 1,
+                        PipeTransmissionMode.Byte,
+                        PipeOptions.Asynchronous);
+                    await namedPipeServer.WaitForConnectionAsync(cts.Token);
+
+                    var buffer = new byte[16]; // Only need enough for the length of a PID, 16 should be plenty
+                    var length = await namedPipeServer.ReadAsync(buffer, cts.Token);
+                    var pid = BitConverter.ToInt32(new ReadOnlySpan<byte>(buffer).Slice(0, length));
+
+                    // TODO how can we pass this info into other transports (QUIC) s.t. it duplicates rather than creates?
+
+                    // Send http socket
+                    var httpSocketInfo = httpListenSocket.DuplicateSocketWindows(pid);
+                    await namedPipeServer.WriteAsync(BitConverter.GetBytes(httpSocketInfo.ProtocolInformation.Length));
+                    await namedPipeServer.WriteAsync(httpSocketInfo.ProtocolInformation, cts.Token);
+
+                    // Send https socket
+                    var httpsSocketInfo = httpsListenSocket.DuplicateSocketWindows(pid);
+                    await namedPipeServer.WriteAsync(BitConverter.GetBytes(httpsSocketInfo.ProtocolInformation.Length));
+                    await namedPipeServer.WriteAsync(httpsSocketInfo.ProtocolInformation, cts.Token);
+
+                    var backendProcess = Process.GetProcessById(pid);
+
+                    await backendProcess.WaitForExitAsync(cts.Token);
+                }
+                catch (Exception)
+                {
+                    // Ignore exceptions for now.
+                    // TODO enable a debug log mode.
+                    break;
+                }
             }
         }
 
